ec55978579c144cc817399b09ba8dd413f5c0af1
Payden Hoskins <paydenhoskins@gmail.com>
2025-03-02 14:07:00 -0700

    Header and Rename

---
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -0,0 +1,17 @@
+﻿'Payden Hoskins
+'RCET2265
+'Spring 2025
+'ShuffleTheDeck
+'https://github.com/PaydenHoskins/ShuffleTheDeck.git
+
+Option Compare Text
+Option Explicit On
+Option Strict On
+
+Module ShuffleTheDeck
+
+    Sub Main()
+
+    End Sub
+
+End Module
